d93f565a3a5d74028e623bf8900116853bf51e33
testermi66 <testermi66@gmail.com>
2026-04-20 00:06:03 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', event => {
+    console.log('Service Worker установлен');
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(fetch(event.request));
+});
